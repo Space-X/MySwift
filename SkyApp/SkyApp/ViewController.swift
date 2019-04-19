ab02659b7c54c71541ffe9747b30fc8cf970a2e0
dichao <158566751@qq.com>
2019-04-19 16:55:48 +0800

    init project

---
+++ SkyApp/SkyApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SkyApp
+//
+//  Created by pku on 2019/4/19.
+//  Copyright © 2019年 Space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightText;
+    }
+
+
+}
+
